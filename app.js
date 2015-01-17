0763d8a886fe455a459b90c07300c04cf52ec9e7
exfoxz <chanhnp@gmail.com>
2015-01-17 10:10:02 -0500

    update header and charts

--- app.js
+++ app.js
@@ -18,7 +18,49 @@ app.config(function($mdThemingProvider) {
         'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
     })
 });
-app.controller('AppCtrl', function($scope, $timeout, $mdSidenav, $log, $mdDialog) {
+app.controller('AppCtrl', function($scope, $timeout, $mdSidenav, $log, $mdDialog, $mdToast) {
+    
+    $scope.fillMoneyGraph = function(){
+        var chart = c3.generate({
+            data: {
+                x: 'x',
+                //        xFormat: '%Y%m%d', // 'xFormat' can be used as custom format of 'x'
+                columns: [
+                    ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06', '2013-01-07', '2013-01-08', '2013-01-09', '2013-01-10', '2013-01-11', '2013-01-12', '2013-01-13', '2013-01-14', '2013-01-15', '2013-01-16'],
+                    //            ['x', '20130101', '20130102', '20130103', '20130104', '20130105', '20130106'],
+                    ['data1', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250]
+                ],
+                colors: {
+                    data1: '#ffffff',
+                }
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        format: '%Y-%m-%d'
+                    }
+                },
+                y : {
+                    min: 0,
+                    padding: {bottom: 0},
+                    tick: {
+                        count: 3,
+                        format: d3.format("$,")
+                        //                format: function (d) { return "$" + d; }
+                    }
+                }
+            },
+            bindto: '#money-graph',
+            grid: {
+                x: {
+                    show: true
+                }
+            }
+        });
+    };
+    
+    $scope.fillMoneyGraph();
     $scope.toggleLeft = function() {
         $mdSidenav('left').toggle()
         .then(function(){
@@ -76,14 +118,28 @@ app.controller('AppCtrl', function($scope, $timeout, $mdSidenav, $log, $mdDialog
                     // iris data from R
                     columns: obj.data,
                     type : 'pie',
-                    onclick: function (d, i) { console.log("onclick", d, i); },
-                    onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-                    onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+                    colors: {
+                        bond: '#00897b',
+                        stock: '#00796b'
+                    },
+//                    onclick: function (d, i) { console.log("onclick", d, i); },
+//                    onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+//                    onmouseout: function (d, i) { console.log("onmouseout", d, i); }
                 },
                 bindto: '#' + obj.name,
             }); 
         })
     }, 100)
+    
+    $scope.showCustomToast = function(port) {
+        $mdToast.show({
+            controller: 'ToastCtrl',
+            templateUrl: 'toast-template.html',
+            hideDelay: 5000,
+            position: "bottom left",
+            locals: {portfolio: port}
+        });
+    };
     
     /* Dialog */
     $scope.showAdvanced = function(ev) {
@@ -92,25 +148,84 @@ app.controller('AppCtrl', function($scope, $timeout, $mdSidenav, $log, $mdDialog
             templateUrl: 'dialog-invest.html',
             targetEvent: ev,
         })
-        .then(function(answer) {
-            $scope.alert = 'You said the information was "' + answer + '".';
+        .then(function(port) {
+            console.log("MainCtrl, you bought", port);
+            /* Make a toast */
+            $scope.showCustomToast(port);
         }, function() {
             $scope.alert = 'You cancelled the dialog.';
         });
     };
     
     function DialogController($scope, $mdDialog) {
+        $scope.data = {};
         $scope.hide = function() {
             $mdDialog.hide();
         };
         $scope.cancel = function() {
             $mdDialog.cancel();
         };
-        $scope.answer = function(answer) {
-            $mdDialog.hide(answer);
+        $scope.purchase = function(port) {
+            if(!port) {
+                $scope.alert = "Please choose a portfolio";
+                setTimeout(function(){
+                    $scope.$apply(function(){
+                        $scope.alert = "";
+                    })
+                    
+                }, 2000);
+            } else {
+                $scope.inTransaction = !$scope.inTransaction;
+                setTimeout(function(){
+                    /* Call out to server to purchase portfoli o*/
+                    console.log('Bought', port);
+                    $mdDialog.hide(port);    
+                }, 2000)
+            }
         };
+        
+        $scope.portfolios = [
+            {
+                face : '/img/rec1.png',
+                portfolio: 'Portfolio 1',
+                confidence: '20',
+                income: '5%',
+                notes: " I'll be in your neighborhood doing errands"
+            },
+            {
+                face : '/img/rec2.png',
+                portfolio: 'Portfolio 2',
+                confidence: '40',
+                income: '8%',
+                notes: " I'll be in your neighborhood doing errands"
+            },
+            {
+                face : '/img/rec3.png',
+                portfolio: 'Portfolio 3',
+                confidence: '30',
+                income: '10%',
+                notes: " I'll be in your neighborhood doing errands"
+            }
+            
+        ];
+        $scope.currentPortfolio = null;
+        $scope.currentPortIndex = null;
+        $scope.updateSelection = function(index, portfolios) {
+            if($scope.currentPortIndex == index) {
+                $scope.currentPortfolio = null;
+                $scope.currentPortIndex = null;
+            } else {
+                $scope.currentPortfolio = portfolios[index];
+                $scope.currentPortIndex = index;
+            }
+            
+            portfolios.forEach(function(item, pos){
+                if(pos!= index) {
+                    item.checked = false;
+                }
+            })
+        }
     }
-
 })
 .controller('LeftCtrl', function($scope, $timeout, $mdSidenav, $log) {
     $scope.close = function() {
@@ -127,4 +242,10 @@ app.controller('AppCtrl', function($scope, $timeout, $mdSidenav, $log, $mdDialog
             $log.debug("close RIGHT is done");
         });
     };
+})
+.controller('ToastCtrl', function($scope, $mdToast, portfolio){
+    $scope.closeToast = function() {
+        $mdToast.hide();
+    };
+    $scope.portfolio = portfolio;
 });
