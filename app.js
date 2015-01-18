190e57709f3c342371e5521eb0adc5547b18b47d
exfoxz <chanhnp@gmail.com>
2015-01-18 00:41:43 -0500

    done?

--- app.js
+++ app.js
@@ -20,34 +20,42 @@ app.config(function($mdThemingProvider) {
 });
 app.controller('AppCtrl', function($scope, $timeout, $mdSidenav, $log, $mdDialog, $mdToast, $window) {
     
+//    $scope.interest = ['interest', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250];
+    $scope.dates = ['x', '2013-11-01', '2013-11-02', '2013-11-03', '2013-11-04', '2013-11-05', '2013-11-06', '2013-11-07', '2013-11-08', '2013-11-09', '2013-11-10', '2013-11-11', '2013-11-12', '2013-11-13', '2013-11-14', '2013-11-15', '2013-11-16', '2013-11-17', '2013-11-18', '2013-11-19', '2013-11-20', '2013-11-21', '2013-11-22', '2013-11-23', '2013-11-24', '2013-11-25', '2013-11-26', '2013-11-27', '2013-11-28', '2013-11-29', '2013-11-30', '2013-12-01', '2013-12-02', '2013-12-03', '2013-12-04', '2013-12-05', '2013-12-06', '2013-12-07', '2013-12-08', '2013-12-09', '2013-12-10', '2013-12-11', '2013-12-12', '2013-12-13', '2013-12-14', '2013-12-15', '2013-12-16', '2013-12-17', '2013-12-18', '2013-12-19', '2013-12-20', '2013-12-21', '2013-12-22', '2013-12-23', '2013-12-24', '2013-12-25', '2013-12-26', '2013-12-27', '2013-12-28', '2013-12-29', '2013-12-30', '2013-12-31'];
+    $scope.portData= [
+        ['interest', 34.39, 34.99, 35.57, 35.42, 34.94, 34.74, 34.67, 34.78, 34.79, 34.57, 34.78, 34.88, 35.16, 35.04, 35.2726, 35.37, 35.47, 35.66, 35.86, 36.19, 36.31, 36.42, 36.81, 36.72, 36.502, 36.7786, 37.01, 37.17, 37.1144, 37.012, 36.74, 36.59, 36.88, 37.54, 37.82, 38.12, 38.05, 37.96, 38.03, 38.14, 38.27, 37.95], ['interest', 25.21, 25.91, 26.4, 26.38, 26.14, 25.77, 25.55, 25.76, 25.64, 25.4, 25.69, 25.56, 25.85, 25.91, 26.17, 26.04, 26.27, 26.47, 26.59, 26.83, 26.76, 26.84, 27.3, 27.1, 27.13, 27.28, 27.38, 27.44, 27.3, 27.2, 27.15, 27.12, 27.01, 27.86, 28.16, 28.16, 28.39, 28.43, 28.34, 28.52, 28.66, 28.52],
+        ['interest', 105.00, 106.03, 107.18, 106.18, 104.97, 105.29, 106.29, 105.79, 105.20, 106.09, 105.40, 106.56, 106.75, 105.68, 105.88, 106.60, 106.43, 106.31, 107.94, 108.51, 109.39, 109.72, 109.56, 109.95, 108.64, 110.03, 110.60, 111.53, 111.54, 110.94, 109.87, 109.12, 111.77, 112.11, 112.14, 114.46, 113.29, 112.44, 113.05, 112.94, 112.49, 110.75]
+    ]
     $scope.fillMoneyGraph = function(){
-        var chart = c3.generate({
+        $scope.moneyGraph = c3.generate({
             data: {
                 x: 'x',
                 //        xFormat: '%Y%m%d', // 'xFormat' can be used as custom format of 'x'
+                
                 columns: [
-                    ['x', '2013-01-01', '2013-01-02', '2013-01-03', '2013-01-04', '2013-01-05', '2013-01-06', '2013-01-07', '2013-01-08', '2013-01-09', '2013-01-10', '2013-01-11', '2013-01-12', '2013-01-13', '2013-01-14', '2013-01-15', '2013-01-16'],
-                    //            ['x', '20130101', '20130102', '20130103', '20130104', '20130105', '20130106'],
-                    ['data1', 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250, 30, 200, 100, 400, 150, 250]
+                    $scope.dates,
+                    $scope.portData[0]
+                    
                 ],
                 colors: {
-                    data1: '#000000',
+                    interest: '#20B2AA',
+                    growth: '#ff0000'
                 }
             },
             axis: {
                 x: {
                     type: 'timeseries',
                     tick: {
-                        count: 4,
+                        count:5,
                         format: '%Y-%m-%d'
                     }
                 },
                 y : {
-                    min: 0,
-                    padding: {bottom: 0},
+                    min: 30,
+                    padding: {},
                     tick: {
                         count: 3,
-                        format: d3.format("$,")
+                        format: function (d) { return "$" + Math.ceil(d); }
                         //                format: function (d) { return "$" + d; }
                     }
                 }
@@ -84,31 +92,88 @@ app.controller('AppCtrl', function($scope, $timeout, $mdSidenav, $log, $mdDialog
             id: 'p1',
             face : '/img/rec1.png',
             portfolio: 'Portfolio 1',
-            confidence: '20',
-            income: '5%',
-            notes: " I'll be in your neighborhood doing errands",
+            stocks: ['GGP', 'AVB', 'AIV', 'HCN'],
+            confidence: 90,
+            income: 14.082,
             current: 0
         },
         {
             id: 'p2',
             face : '/img/rec2.png',
             portfolio: 'Portfolio 2',
-            confidence: '40',
-            income: '8%',
-            notes: " I'll be in your neighborhood doing errands",
+            stocks: ['ESS', 'GGP', 'AVB', 'AIV'],
+            confidence: 87,
+            income: 14.07,
             current: 0
         },
         {
             id: 'p3',
             face : '/img/rec3.png',
             portfolio: 'Portfolio 3',
-            confidence: '30',
-            income: '10%',
-            notes: " I'll be in your neighborhood doing errands",
+            stocks: ['ESS', 'AVB', 'AIV', 'HCN'],
+            confidence: 71,
+            income: 14.078,
             current: 0
         }
+        
 
     ];
+    
+    /* Add new line to line graph */
+    $scope.shout = function(index){
+        var currentData = $scope.portData[index]
+        var avg = 0;
+        var min = currentData[1];
+        for(var j = 0; j < currentData.length; j++) {
+            if(min > currentData[j])
+                min = currentData[j];
+            avg+= currentData[j];
+        }
+        avg = avg / (currentData.length - 1);
+        
+        $scope.moneyGraph = c3.generate({
+            data: {
+                x: 'x',
+                //        xFormat: '%Y%m%d', // 'xFormat' can be used as custom format of 'x'
+
+                columns: [
+                    $scope.dates,
+                    $scope.portData[index],
+                ],
+                colors: {
+                    interest: '#20B2AA',
+                    growth: '#ff0000'
+                }
+            },
+            axis: {
+                x: {
+                    type: 'timeseries',
+                    tick: {
+                        count:5,
+                        format: '%Y-%m-%d'
+                    }
+                },
+                y : {
+                    min: min - 5,
+                    padding: {},
+                    tick: {
+                        count: 3,
+//                        format: d3.format("$,")
+                        format: function (d) { return "$" + Math.ceil(d); }
+                    }
+                }
+            },
+            bindto: '#money-graph',
+            grid: {
+                x: {
+                    show: true
+                }
+            },
+            legend: {
+                show: false
+            }
+        });
+    }
     
     $scope.goLanding = function(){
         console.log("go landing");
@@ -120,47 +185,56 @@ app.controller('AppCtrl', function($scope, $timeout, $mdSidenav, $log, $mdDialog
         //        $location.path( '../index.html' );
         $window.location.href = "main.html";
     }
-    
-    $scope.chartData =  [
+    $scope.portfolios = [
         {
-            name: "chart1",
+            id: 'p1',
+            name: 'chart1',
+            face : '/img/rec1.png',
+            portfolio: 'Portfolio 1',
+            stocks: ['GGP', 'AVB', 'AIV', 'HCN'],
+            confidence: 90,
+            income: 14.082,
+            current: 0,
             data: [
                 ['bond', 10],
                 ['stock', 90],
-            ],
-            portfolio: "Portfolio 1",
-            confidence: 20,
-            term: "Short term",
-            income: "5%"
+            ]
         },
         {
-            name: "chart2",
+            id: 'p2',
+            name: 'chart2',
+            face : '/img/rec2.png',
+            portfolio: 'Portfolio 2',
+            stocks: ['ESS', 'GGP', 'AVB', 'AIV'],
+            confidence: 87,
+            income: 14.07,
+            current: 0,
             data: [
                 ['bond', 20],
                 ['stock', 80],
-            ],
-            portfolio: "Portfolio 2",
-            confidence: 40,
-            term: "Middle term",
-            income: "8%"
+            ]
         },
         {
-            name: "chart3",
+            id: 'p3',
+            name: 'chart3',
+            face : '/img/rec3.png',
+            portfolio: 'Portfolio 3',
+            stocks: ['ESS', 'AVB', 'AIV', 'HCN'],
+            confidence: 71,
+            income: 14.078,
+            current: 0,
             data: [
                 ['bond', 30],
                 ['stock', 70],
-            ],
-            portfolio: "Portfolio 3",
-            confidence: 30,
-            term: "Long term",
-            income: "10%"
-        }   
+            ]
+        }
+
     ];
     $scope.charts = {};
     /* Make charts */
     setTimeout(function(){
-        $scope.chartData.forEach(function(obj){
-            $scope.charts[obj.name] = c3.generate({
+        $scope.portfolios.forEach(function(obj){
+            $scope.charts[obj.id] = c3.generate({
                 data: {
                     // iris data from R
                     columns: obj.data,
@@ -168,12 +242,14 @@ app.controller('AppCtrl', function($scope, $timeout, $mdSidenav, $log, $mdDialog
                     colors: {
                         bond: '#00897b',
                         stock: '#00796b'
-                    },
-//                    onclick: function (d, i) { console.log("onclick", d, i); },
+                    }
+//                    onclick: function (d, i) { 
+//                        console.log("onclick", d, i);
+//                    },
 //                    onmouseover: function (d, i) { console.log("onmouseover", d, i); },
 //                    onmouseout: function (d, i) { console.log("onmouseout", d, i); }
                 },
-                bindto: '#' + obj.name,
+                bindto: '#' + obj.name
             }); 
         })
     }, 100)
